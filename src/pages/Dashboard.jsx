9e13238a1655eb79fe3e69899209082c7db9cb7e
Mohit Rathee <mohitrathee051@gmail.com>
2024-04-03 13:33:51 +0530

    dashboard css

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,10 +4,11 @@ import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import styles from "./Dashboard.module.css";
 
 
 
-function Dashboard({  }) {
+function Dashboard({ }) {
     const navigate = useNavigate();
     const [tasks, setTasks] = useState([
         { id: 1, text: "Drink 1 gallon of water", completed: false },
@@ -50,21 +51,24 @@ function Dashboard({  }) {
 
 
     return (
-        <div>
+        <div className={styles.fullscreen}>
             <Navbar />
-            <h1>Dashboard</h1>
-            <ul>
-                {tasks.map(task => (
-                    <li key={task.id}>
-                        <input
-                            type="checkbox"
-                            checked={task.completed}
-                            onChange={() => handleToggleComplete(task.id)}
-                        />
-                        <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.text}</span>
-                    </li>
-                ))}
-            </ul>
+            <div className={styles.todo}>
+
+                <h1>TO-Dos:</h1>
+                <ul>
+                    {tasks.map(task => (
+                        <li key={task.id}>
+                            <input
+                                type="checkbox"
+                                checked={task.completed}
+                                onChange={() => handleToggleComplete(task.id)}
+                            />
+                            <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.text}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
